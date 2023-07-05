9b5cdd77beebb436115c47278b072d0e46cf5592
NingYang <nyang@yipitdata.com>
2023-07-05 12:28:57 +0800

    add transmitJSON

--- ios/RtnTurboModulesHelper.mm
+++ ios/RtnTurboModulesHelper.mm
@@ -5,13 +5,22 @@
 
 RCT_EXPORT_MODULE(RtnTurboModulesHelper)
 
-RCT_REMAP_METHOD(transmitString, strA:(NSString *)a
+RCT_REMAP_METHOD(transmitString, str:(NSString *)str
                 withResolver:(RCTPromiseResolveBlock) resolve
                 withRejecter:(RCTPromiseRejectBlock) reject)
 {
-    NSString *result = [NSString stringWithFormat:@"%@",a];
+    NSString *result = [NSString stringWithFormat:@"%@",str];
     resolve(result);
 }
+
+RCT_REMAP_METHOD(transmitJSON, data:(NSArray *)data 
+                withResolver:(RCTPromiseResolveBlock) resolve
+                withRejecter:(RCTPromiseRejectBlock) reject)
+{
+  NSArray *result = [[NSArray alloc] initWithArray:data];
+  resolve(result);
+}
+
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
--- src/NativeHelper.ts
+++ src/NativeHelper.ts
@@ -1,7 +1,9 @@
 import { TurboModule, TurboModuleRegistry } from "react-native";
+import { Thread } from "./types";
 
 export interface Spec extends TurboModule {
   transmitString(string: string): Promise<string>;
+  transmitJSON(threads: Thread[]): Promise<Thread[]>;
 }
 
 export default TurboModuleRegistry.get<Spec>(
